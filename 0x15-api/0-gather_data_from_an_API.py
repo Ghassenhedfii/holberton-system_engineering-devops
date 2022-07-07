681c5d6b89ef49b9826c8c3450123a8ee8ebbe8a
Ghassenhedfii <3681@holbertonschool.com>
2022-07-07 04:32:22 +0200

    gather data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -20,4 +20,3 @@ if __name__ == '__main__':
     print("Employee {} is done with tasks({}/{}):".
           format(user.get('name'), len(completed_tasks), len(todo)))
     print("\n".join("\t {}".format(task) for task in completed_tasks))
-    
